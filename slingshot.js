c8e7549d3cdb67f61d102139d5fb578d86ebebd9
Aditya783123 <72315477+Aditya783123@users.noreply.github.com>
2020-11-25 13:07:37 -0800

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,34 @@
+
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        const world = Matter.World; 
+        //loading the 3 images of slingshot, 2 of which are catapult pictures, and the third one is the rectangular patch
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+
+        //displaying the catapult img's
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+           
+            stroke(48, 22, 8);
+        }
+    }
+    
+}
